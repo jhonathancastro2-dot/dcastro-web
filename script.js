509ce994ea12bf4a0d027b1474fe79b945e1bd3b
jhonathancastro2-dot <jhonathancastro2@gmail.com>
2026-07-18 13:51:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-function verOpciones(producto) {
+function verOpciones(producto){
+    document.getElementById("ventanaMenu").style.display="flex";
+    document.getElementById("tituloProducto").innerHTML=producto;
+}
 
-    let opcion = prompt(
-        "🍗 " + producto +
-        "\n\nSelecciona una opción:" +
-        "\n1. 6 Alitas - $18.000" +
-        "\n2. 12 Alitas - $32.000" +
-        "\n3. 24 Alitas - $58.000"
+function cerrarVentana(){
+    document.getElementById("ventanaMenu").style.display="none";
+}
+
+function pedirWhatsApp(opcion){
+    window.open(
+        "https://wa.me/573206564360?text=Hola D'CASTRO, quiero pedir " + encodeURIComponent(opcion),
+        "_blank"
     );
-
-    if(opcion == "1"){
-        window.open("https://wa.me/573206564360?text=Hola%20D'CASTRO,%20quiero%206%20Alitas%20BBQ");
-    }
-
-    else if(opcion == "2"){
-        window.open("https://wa.me/573206564360?text=Hola%20D'CASTRO,%20quiero%2012%20Alitas%20BBQ");
-    }
-
-    else if(opcion == "3"){
-        window.open("https://wa.me/573206564360?text=Hola%20D'CASTRO,%20quiero%2024%20Alitas%20BBQ");
-    }
-
 }
