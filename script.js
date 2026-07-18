ced0d2bfa93da5759ab25a648a5ddf990d9c0dd7
jhonathancastro2-dot <jhonathancastro2@gmail.com>
2026-07-18 13:29:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function verOpciones(producto) {
-    alert("Elegiste: " + producto);
+    let opcion = prompt(
+        "🍗 " + producto +
+        "\n\nSelecciona una opción:" +
+        "\n1. 6 Alitas - $18.000" +
+        "\n2. 12 Alitas - $32.000" +
+        "\n3. 24 Alitas - $58.000"
+    );
+
+    if (opcion !== null) {
+        alert("Elegiste la opción " + opcion);
+    }
 }
