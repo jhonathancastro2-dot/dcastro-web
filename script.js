97d3c5675d5998da496e6721ba2797284ceeb52b
jhonathancastro2-dot <jhonathancastro2@gmail.com>
2026-08-26 11:23:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -63,11 +63,38 @@ if(producto=="Perros Calientes"){
 }
     if(producto=="Sandwiches"){
     botones=`
-   <button onclick="agregarCarrito('Sándwich Cubano',13000)">🥪 Sándwich Cubano - $13.000</button>
 
-<button onclick="agregarCarrito('Sándwich Especial',16000)">🥪 Sándwich Especial - $16.000</button>
+    <div class="producto-opcion">
+        <h3>🥪 Sándwich Clásico - $13.000</h3>
+        <p>
+            Pan • Queso • Jamón • Batavia • Tomate
+        </p>
+        <button onclick="agregarCarrito('Sándwich Clásico',13000)">
+            🛒 Agregar al carrito
+        </button>
+    </div>
 
-<button onclick="agregarCarrito('Sándwich Doble',20000)">🥪 Sándwich Doble - $20.000</button>
+    <div class="producto-opcion">
+        <h3>🥪 Sándwich Especial - $16.000</h3>
+        <p>
+            Pan • Queso • Jamón • Batavia • Tomate • Ripio • Pollo desmechado
+        </p>
+        <button onclick="agregarCarrito('Sándwich Especial',16000)">
+            🛒 Agregar al carrito
+        </button>
+    </div>
+
+    <div class="producto-opcion">
+        <h3>🥪 Sándwich Doble - $20.000</h3>
+        <p>
+            Pan • Queso doble • Jamón • Batavia • Pollo desmechado •
+            Carne desmechada • Tomate
+        </p>
+        <button onclick="agregarCarrito('Sándwich Doble',20000)">
+            🛒 Agregar al carrito
+        </button>
+    </div>
+
     `;
 }
     if(producto=="Salchipapas"){
